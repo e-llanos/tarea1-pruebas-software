8a12ea3d29108b2ef450b2c3c5c05a18fff7f75b
Esteban Llanos <e.llanos01@ufromail.cl>
2023-09-25 16:53:05 -0300

    feat: Add variable files

---
+++ src/config/automobileConstants.js
@@ -0,0 +1,12 @@
+export const brands = ['Ford', 'Nissan', 'Toyota', 'Honda', 'Chevrolet', 'Volkswagen', 'Hyundai', 'Kia', 'Mazda'];
+export const years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023];
+export const colors = ['Red', 'Blue', 'Black', 'White', 'Gray', 'Silver', 'Green', 'Yellow'];
+export const turbos = ['Yes', 'No'];
+export const types = ['Sedan', 'Truck', 'SUV'];
+export const engines = {
+    'Sedan': ['1.4cc', '1.6cc', '2.0cc'],
+    'Truck': ['2.4cc', '3.0cc', '4.0cc'],
+    'SUV': ['1.8cc', '2.2cc', '2.8cc'],
+};
+export const truckCabins = ['1', '2'];
+export const suvSunroofs = ['Yes', 'No'];
---
+++ src/config/configPaths.js
@@ -0,0 +1,1 @@
+export const filePathData = './data/automobiles.json';
